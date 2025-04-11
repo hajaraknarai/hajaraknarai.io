707be0f74f7fad9578fc134cb7d1a0a6bd1e3e20
hajaraknarai <hajaraknarai@gmail.com>
2025-04-11 03:46:52 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+let num1 = parseFloat(prompt("Entrez le premier nombre :"));
+let num2 = parseFloat(prompt("Entrez le deuxième nombre :"));
+alert("Somme : " + (num1 + num2) + "\nProduit : " + (num1 * num2));
+let prenom = prompt("Entrez votre prénom :");
+let nom = prompt("Entrez votre nom :");
+console.log("Bonjour " + prenom + " " + nom);
+let age = parseInt(prompt("Entrez votre âge :"));
+let message = (age > 18) ? "Vous êtes majeur." : "Vous êtes mineur.";
+alert(message);
